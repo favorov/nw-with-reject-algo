55110b5967b1e57de4487f1264d4882de7a7d2b7
Alexander Favorov <favorov@sensi.org>
2019-05-16 02:29:50 -0400

    command line for mwwreject

--- cmd/nwalgo/main.go
+++ cmd/nwalgo/main.go
@@ -1,4 +1,5 @@
-// Copyright 2015 Andrew E. Bruno. All rights reserved.
+// Copyright 2015 Andrew E. Bruno, 2019 A. Favorov. 
+// All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
@@ -8,8 +9,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-
-	"github.com/aebruno/nwalgo"
+	"github.com/favorov/nwwrej"
 )
 
 var seq1 = flag.String("seq1", "", "first sequence")
@@ -17,6 +17,7 @@ var seq2 = flag.String("seq2", "", "second sequence")
 var match = flag.Int("match", 1, "match score")
 var mismatch = flag.Int("mismatch", -1, "mismatch score")
 var gap = flag.Int("gap", -1, "gap penalty")
+var threshold = flag.Int("threshold", 0, "threshold to reject")
 
 func main() {
 	flag.Parse()
@@ -24,6 +25,10 @@ func main() {
 		log.Fatal("Please provide 2 sequences to align. See nwalgo --help")
 	}
 
-	aln1, aln2, score := nwalgo.Align(*seq1, *seq2, *match, *mismatch, *gap)
-	fmt.Printf("%s\n%s\nScore: %d\n", aln1, aln2, score)
+	aln1, aln2, score,ok := nwwreject.Align(*seq1, *seq2, *match, *mismatch, *gap,*threshold)
+	if ok {
+		fmt.Printf("%s\n%s\nScore: %d\n", aln1, aln2, score)
+	} else {
+		fmt.Printf("Sequences differ too much.\n")
+	}
 }
