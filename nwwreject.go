274eee46d17af4bce469c07c5d1ca51c3622439c
Alexander Favorov <favorov@sensi.org>
2019-05-21 02:18:14 -0400

    added debug line

--- nwwreject.go
+++ nwwreject.go
@@ -106,7 +106,9 @@ func Align(a, b string, mismatch, gap, threshold int) (alignA, alignB string, di
 			}
 			
 			f[idx(i, j, bLen)] = min
-
+			
+			log.Println(i,j,min.threshold,we_broke_at)
+			
 			if min > threshold {
 				pointer[idx(i, j, bLen)] = Stop //the value is set already
 				if nonstop_already_found {
